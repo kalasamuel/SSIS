5a9ddfd0a244b352e8f49798ee315d9b0856add1
Melissa Violet <melissavioletvs@gmail.com>
2025-10-17 10:11:56 +0300

    gotten app password for the email sending

--- supermarket/settings.py
+++ supermarket/settings.py
@@ -130,3 +130,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'melissavioletvs@gmail.com'
+EMAIL_HOST_PASSWORD = 'bwkw hdhq bqej pgzb'  # Not your normal Gmail password!
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
