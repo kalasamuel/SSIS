bdf6c86868a4086ad3be876964fbd36a65fe54bd
Melissa Violet <melissavioletvs@gmail.com>
2025-10-17 12:48:26 +0300

    proper name reference on PO page

--- supermarket/settings.py
+++ supermarket/settings.py
@@ -135,5 +135,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'melissavioletvs@gmail.com'
-EMAIL_HOST_PASSWORD = 'bwkw hdhq bqej pgzb'  # Not your normal Gmail password!
+EMAIL_HOST_PASSWORD = 'bwkw hdhq bqej pgzb'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
